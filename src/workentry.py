240958c72c0b1fe74718e666ce9b02591f2f5b70
afuerstenau <alex@fuerstenau-clan.de>
2015-11-27 22:24:39 +0100

    several modifications

--- src/workentry.py
+++ src/workentry.py
@@ -1,18 +1,21 @@
 import time
-import sys
-
 
 class Workentry:
     def __init__(self, description, starttime, endtime, lunchbreak=0):
             self._description = description
             self._starttime = self.string_to_time(starttime)
             self._endtime = self.string_to_time(endtime)
+            self._lunchbreak = "00:00"
+            self._lunchbreak_as_number = 0
             if lunchbreak=="30":
                 self._lunchbreak = "01:30"
+                self._lunchbreak_as_number = 0.5
             elif lunchbreak=="1":
                 self._lunchbreak = "01:00"
-            elif lunchbreak == "1,5":
+                self._lunchbreak_as_number = 1
+            elif lunchbreak == "1.5":
                 self._lunchbreak = "01:30"
+                self._lunchbreak_as_number = 1.5
 
 
     def description(self):
@@ -26,6 +29,9 @@ class Workentry:
 
     def lunchbreak(self):
         return self._lunchbreak
+
+    def working_time(self):
+        return (time.mktime(self._endtime)-time.mktime(self._starttime))/3600-self._lunchbreak_as_number
 
     def string_to_time(self, string):
         y,d,m,H,M,S,ZS,HS,TS = time.localtime()
@@ -42,9 +48,3 @@ class Workentry:
         TS = 0
         time_tupel = y,d,m,H,M,S,ZS,HS,TS
         return time_tupel
-
-if __name__ == '__main__':
-    workentry = Workentry(sys.argv[1], sys.argv[2], sys.argv[3])
-    sys.stdout.write(workentry.description() + " " + workentry.starttime() + "-" + workentry.endtime())
-    sys.stdout.flush()
-    sys.exit(0)
--- test/workentry_test.py
+++ test/workentry_test.py
@@ -9,13 +9,16 @@ class TestStringMethods(unittest.TestCase):
       self.assertEqual(workentry.description(), "listen to podcast")
       self.assertEqual(workentry.starttime(), "08:00")
       self.assertEqual(workentry.endtime(), "17:30")
+      self.assertEqual(workentry.lunchbreak(), "00:00")
+      self.assertEqual(workentry.working_time(), 9.5)
 
-  def test_create_workentry_with_date(self):
-      workentry = Workentry("listen to podcast", "09:30", "18", "1,5")
+  def test_create_workentry_with_complex_date(self):
+      workentry = Workentry("listen to podcast", "09:30", "18", "1.5")
       self.assertEqual(workentry.description(), "listen to podcast")
       self.assertEqual(workentry.starttime(), "09:30")
       self.assertEqual(workentry.endtime(), "18:00")
       self.assertEqual(workentry.lunchbreak(), "01:30")
+      self.assertEqual(workentry.working_time(), 7)
 
 if __name__ == '__main__':
     unittest.main()
