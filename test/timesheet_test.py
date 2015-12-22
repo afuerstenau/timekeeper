6f2548697fbe267061110fd22f50729fddb689d0
afuerstenau <alex@fuerstenau-clan.de>
2015-12-22 09:16:06 +0100

    switched tests to run with nosetest

--- test/timesheet_test.py
+++ test/timesheet_test.py
@@ -17,23 +17,23 @@ class TestTimesheet(unittest.TestCase):
         os.remove("test.xlsx")
 
     def test_timesheet_summary(self):
-        timesheet = Timesheet("timekeeper/test/test_timesheet_summary.xlsx")
+        timesheet = Timesheet("test/test_timesheet_summary.xlsx")
         self.assertEqual(timesheet.calculate_sum_working_time(), 26.5)
 
     def test_set_sum(self):
-        timesheet = Timesheet("timekeeper/test/test_timesheet_summary.xlsx")
+        timesheet = Timesheet("test/test_timesheet_summary.xlsx")
         timesheet.set_sum_working_time(5)
 
     def test_get_sum(self):
-        timesheet = Timesheet("timekeeper/test/test_timesheet_summary.xlsx")
+        timesheet = Timesheet("test/test_timesheet_summary.xlsx")
         self.assertEqual(timesheet.get_sum_working_time(), 26.5)
 
     def test_get_diff_normative_working_time(self):
-        timesheet = Timesheet("timekeeper/test/test_timesheet_summary.xlsx")
+        timesheet = Timesheet("test/test_timesheet_summary.xlsx")
         self.assertEqual(timesheet.get_diff_normative_actual_working_time_in_hours(), 69.5)
 
     def test_get_reamining_working_days(self):
-        timesheet = Timesheet("timekeeper/test/test_timesheet_summary.xlsx")
+        timesheet = Timesheet("test/test_timesheet_summary.xlsx")
         self.assertEqual(timesheet.get_remaining_days(), 8.6875)
 
 
